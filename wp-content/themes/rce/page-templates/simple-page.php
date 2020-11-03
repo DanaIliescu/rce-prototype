7cdf9cd4646865ba13cc22783c8a0da83076877d
Dana Iliescu <dana.iliescu96@gmail.com>
2020-11-03 17:39:36 +0100

    Fix text being cut on the tutorial page and text on front page should be under the image

---
+++ wp-content/themes/rce/page-templates/simple-page.php
@@ -0,0 +1,11 @@
+<?php
+/* Template Name: Simple page */
+?>
+<?php get_header(); ?>
+
+<div class="Row Row--top">
+	<div class="SimplePage">
+		<h2><?php the_title(); ?></h2>
+		<div><?php the_content(); ?></div>
+	</div>
+</div>
